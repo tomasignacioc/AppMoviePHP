bb3fd4746151235b98464b33654e2888eeb17af8
Tomas <tomasignaciocontreras@hotmail.com>
2022-08-19 20:35:57 -0300

    comment route and resource added

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\MovieController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -21,3 +22,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request)
 });
 
 Route::resource('movie', MovieController::class);
+Route::resource('comment', CommentController::class);

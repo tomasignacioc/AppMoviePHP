93d3e475761b889f6c55c91994fd5a2bad848657
Tomas <tomasignaciocontreras@hotmail.com>
2022-09-07 13:23:45 -0300

    register and login endpoints

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\MovieController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,6 +21,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request)
 {
     return $request->user();
 });
+Route::post('register', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'login']);
 
 Route::get('movie/ranking', [MovieController::class, 'ranking']);
 Route::resource('movie', MovieController::class);
